b6c51ab4f98f7d2e57351a4edbcda17125eb7520
Fernando Borrego <fernando.borrego@gmail.com>
2022-10-09 08:10:32 -0500

    Banner e instalacion de dependencia redux

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,18 @@
 import './App.css';
 import SearchBar from './components/SearchBar/SearchBar';
+// import Navbar from './components/Navbar/Navbar';
+import banner from './img/banner.jpg';
 
 function App() {
   return (
     <>
-      <SearchBar />
+      {/* <Navbar title="Prueba Técnica React Rick y Morty"></Navbar> */}
+      <div className="container mx-auto px-4 h-full">.
+        <div className="pb-4 flex justify-center">
+          <img src={banner} alt="Rick y Morty" className="w-1/2" />
+        </div>
+        <SearchBar />
+      </div>
     </>
   );
 }
