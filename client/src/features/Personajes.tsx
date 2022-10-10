7256490af868070f0acd2f1250089ef190ea706f
Fernando Borrego <fernando.borrego@gmail.com>
2022-10-10 00:21:50 -0500

    animaciones tarjetas y fix no encontrados

--- client/src/features/Personajes.tsx
+++ client/src/features/Personajes.tsx
@@ -9,36 +9,43 @@ export interface PersonajesInterface {}
 
 const Personajes : React.FC<PersonajesInterface> = () => {
 	const [searchTerm, setSearchTerm] = useState('');
+	const [error, setError] = useState('');
+	const [hayError, setHayError] = useState(false);
 
-  const {data: personajesAll, isLoading, isSuccess} = useGetPersonajesQuery()
-  const {data: personajeByNombre} = useGetPersonajeByNombreQuery(searchTerm)
+  	const {data: personajesAll, isLoading, isSuccess} = useGetPersonajesQuery()
+  	const {data: personajeByNombre} = useGetPersonajeByNombreQuery(searchTerm)
   
-  let contenidoPersonajes;
-  if (isLoading) {
-    let preloadCards = 12
-    contenidoPersonajes =
-    <>
-      {[...Array(preloadCards)].map((x, n) =>
-        (<PersonajeLoadingCard key={n} />)
-      )}
-    </>
-  }
-  else if (isSuccess) {
-    if (searchTerm && personajeByNombre) {
-      	contenidoPersonajes =
-        	<>
-          		{personajeByNombre.map((personaje) => (
-            		<PersonajeCard key={personaje.id} id={personaje.id} nombre={personaje.nombre} />
-          		))}
-        	</>
-    }
+  	let contenidoPersonajes;
+  	if (isLoading) {
+    	let preloadCards = 12
+    	contenidoPersonajes =
+			<>
+				{[...Array(preloadCards)].map((x, n) =>
+					(<PersonajeLoadingCard key={n} />)
+				)}
+			</>
+  	}
+  	else if (isSuccess) {
+    	if (searchTerm && personajeByNombre) {
+			if (personajeByNombre.length === 0) {
+				setHayError(true);
+				setError('No se encontraron resultados.');
+			}
+			else {
+				contenidoPersonajes =
+					<>
+						{personajeByNombre.map((personaje) => (
+							<PersonajeCard key={personaje.id} id={personaje.id} nombre={personaje.nombre} />
+						))}
+					</>
+			}
+    	}
     else {
-      	if (!personajesAll)
-        	contenidoPersonajes =
-          		<div className="mt-14 text-center text-xl dark:text-white">
-            		No hay personajes
-          		</div>
-      	else
+      	if (personajesAll.length === 0) {
+			setHayError(true);
+        	setError('No se encontraron personajes.');
+		}
+		else
         	contenidoPersonajes =
           		<>
             		{personajesAll.map((personaje) => (
@@ -48,14 +55,21 @@ const Personajes : React.FC<PersonajesInterface> = () => {
     	}
   	}
 	return (
-		<div className="container mx-auto px-4 h-full">.
+		<div className="container mx-auto px-4 h-full">
         	<div className="pb-4 flex justify-center">
           		<img src={banner} alt="Rick y Morty" className="w-1/2" />
         	</div>
         	<SearchBar onSearchTerm={(nombre) => {setSearchTerm(nombre); }} />
-        	<div className="mt-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-        		{contenidoPersonajes}
-        	</div>
+            {
+				hayError && (
+					<div className="mt-14 text-center text-xl dark:text-white">
+						{error}
+					</div>
+				)
+			}
+			<div className="mt-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+				{contenidoPersonajes}
+			</div>
       	</div>
 	);
 };
